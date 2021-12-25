9c7845d6afc5782f1d9bd439809648a07b5e8205
Fatemeh Asgari <fatemeh.asgari@tapsi.cab>
2021-12-25 18:16:16 +0330

    changed and fixed some problems

--- src/ProductItem.js
+++ src/ProductItem.js
@@ -8,13 +8,13 @@ const ProductItem = ({ title, id }) => {
   const addItemToCartHandler = () => {
     dispatch({
       type: "addItem",
-      action: { cartInfo: { title: title, id: id } },
+      cartInfo: { title: title, id: id },
     });
   };
 
   return (
     <li>
-      <section className="item" onClick={addItemToCartHandler}>
+      <section className="product-item" onClick={addItemToCartHandler}>
         <h3 className="item__title">{title}</h3>
       </section>
     </li>
--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,37 @@
 import { createStore } from "redux";
 
-const cartReducer = (state = { cartItems: [] }, action) => {
+const cartReducer = (state ={ cartItems: [] }, action) => {
   if (action.type === "addItem") {
-    const newCartArray = [...state.cartItems];
     const existingItem = state.cartItems.find(
       (item) => item.id === action.cartInfo.id
     );
     if (existingItem) {
+      const newCartArray = [];
       const newQuantity = existingItem.quantity + 1;
-      for (let item in newCartArray) {
-        if (item.id === existingItem.id) {
-          const itemToBeModified = newCartArray.findIndex((item) => item.id === action.cartInfo.id)
-          newCartArray[itemToBeModified].quantity = newQuantity;
+      for (let item of state.cartItems) {
+        if (item.id !== existingItem.id) {
+          newCartArray.push(item);
+        } else {
+          newCartArray.push({
+            id: action.cartInfo.id,
+            title: action.cartInfo.title,
+            quantity: newQuantity,
+          });
         }
       }
+      state = { cartItems: [...newCartArray] };
     }
     if (!existingItem) {
+      const newCartArray = [...state.cartItems];
       newCartArray.push({
         id: action.cartInfo.id,
         title: action.cartInfo.title,
         quantity: 1,
       });
+      state = { cartItems: [...newCartArray] };
     }
-    state = {cartItems: [...newCartArray]};
   }
+  return state;
 };
 
 const store = createStore(cartReducer);
