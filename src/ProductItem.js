f180e78cb9361182a472d97a40c9d4bde89003d9
Fatemeh Asgari <fatemeh.asgari@tapsi.cab>
2021-12-25 15:14:20 +0330

    addItemHandler function added to ProductItem component

--- src/ProductItem.js
+++ src/ProductItem.js
@@ -1,13 +1,24 @@
+import { useDispatch } from "react-redux";
+
 import "./ProductItem.css";
 
-const ProductItem = ({title, id}) => {
-  return(
-    <li >
-      <section className="item">
+const ProductItem = ({ title, id }) => {
+  const dispatch = useDispatch();
+
+  const addItemToCartHandler = () => {
+    dispatch({
+      type: "addItem",
+      action: { cartInfo: { title: title, id: id } },
+    });
+  };
+
+  return (
+    <li>
+      <section className="item" onClick={addItemToCartHandler}>
         <h3 className="item__title">{title}</h3>
       </section>
     </li>
-  )
+  );
 };
 
 export default ProductItem;
